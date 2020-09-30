2623c37586a9fb8eec87d034997e7c3b72490bfc
James Moore <james@knowthen.com>
2020-09-30 10:04:21 -0700

    JWT

---
+++ migrations/sqls/20200930165924-create-type-jwt-up.sql
@@ -0,0 +1,5 @@
+CREATE TYPE learn.jwt AS (
+  role text,
+  person_id integer,
+  exp bigint
+);
--- server.sh
+++ server.sh
@@ -6,4 +6,6 @@ npx postgraphile \
   --allow-explain \
   --watch \
   --owner-connection postgres:///learn_dev \
-  --default-role learn_anonymous
+  --default-role learn_anonymous \
+  --jwt-token-identifier learn.jwt \
+  --jwt-secret secret_for_jwts
